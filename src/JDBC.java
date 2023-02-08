d0c47102a5b7121422612a1f98ac91ec77f44d20
Rohit Shrestha <rohits2013@gmail.com>
2023-02-07 20:25:47 -0700

    Add files via upload

---
+++ src/JDBC.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class JDBC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+/*
+ * import the java.sql.
+ * connection 
+ * driver manager
+ * result 
+ * SQ: exception 
+ * statement 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+ 
